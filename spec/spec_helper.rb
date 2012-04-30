73b9d4d625ac0a92d758538c7661948815d386dc
tomi <tomiacannondale@gmail.com>
2012-04-30 17:19:43 +0900

    Add helper method Gobilin::SpecHelpers#dir

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,13 @@
 # -*- coding: utf-8 -*-
 require 'rspec'
+require File.expand_path('../lib/goblin', File.dirname(__FILE__))
+
+module Goblin::SpecHelpers
+  def dir
+    File.expand_path("data", File.dirname(__FILE__))
+  end
+end
+
+RSpec.configure do |config|
+  config.include Goblin::SpecHelpers
+end
