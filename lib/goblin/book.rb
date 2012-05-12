aba830b8e1c9ad21c93301d89a7d94dc182e369e
tomi <tomiacannondale@gmail.com>
2012-05-12 15:04:09 +0900

    Add Goblin::Book#[]

--- lib/goblin/book.rb
+++ lib/goblin/book.rb
@@ -7,6 +7,16 @@ module Goblin
       Zip::ZipFile.open(file) do |files|
         @content = Nokogiri::XML(files.read("content.xml"))
       end
+      @sheets = @content.xpath("//table:table")
+    end
+
+    def [](name_or_index)
+      if name_or_index.is_a? Numeric
+        sheet = @sheets[name_or_index]
+      else
+        sheet = @sheets.detect{ |i| i["name"] == name_or_index }
+      end
+      sheet && Goblin::Sheet.new(sheet)
     end
 
     def self.open(file)
--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -7,4 +7,27 @@ describe Goblin::Book do
     it { expect { Goblin::Book.open("#{dir}/general_datas.ods") }.to_not raise_error }
   end
 
+  describe "#[]" do
+    before do
+      @book = Goblin::Book.open("#{dir}/general_datas.ods")
+    end
+
+    context "with numeric" do
+      it { @book[0].name.should eq "Sheet1" }
+    end
+
+    context "with sheet name" do
+      it { @book["Sheet1"].name.should eq "Sheet1" }
+    end
+
+    context "with not exist sheet name" do
+      it { @book["not exist"].should be_nil }
+    end
+
+    context "with not exist index" do
+      it { @book[100000].should be_nil }
+    end
+
+  end
+
 end
