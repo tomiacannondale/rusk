5d4230f1d048c2dacde8cd116fe5b5e9eee3e41e
tomi <tomiacannondale@gmail.com>
2013-03-23 19:10:41 +0900

    Use table:number-rows-repeated attribute

--- lib/rusk/sheet.rb
+++ lib/rusk/sheet.rb
@@ -7,16 +7,6 @@ module Rusk
     def initialize(content)
       @content = content
       @cells = []
-      @content.xpath('.//table:table-row').each do |row_range|
-        row_cells = []
-        row_range.xpath(".//table:table-cell|.//table:covered-table-cell").each do |cell|
-          row_cells << Rusk::Cell.new(cell)
-          (cell["table:number-columns-repeated"].to_i - 1).times do
-            row_cells << Rusk::Cell.new(cell)
-          end
-        end
-        @cells << row_cells
-      end
     end
 
     def name
@@ -41,8 +31,31 @@ module Rusk
     end
 
     def each_row
-      @cells.each do |rows|
-        yield rows
+      row_index = 0
+      @content.xpath('.//table:table-row').each_with_index do |row_range, index|
+        if @cells[row_index]
+          yield @cells[row_index]
+          row_index += 1
+          next
+        end
+        cells = row_cells(row_range)
+        yield cells
+        @cells << cells
+        row_index += 1
+
+        rows_repeated = row_range["table:number-rows-repeated"].to_i
+        if rows_repeated > 1
+          base_row_range = row_range
+          (rows_repeated - 1).times do |i|
+            base_row_range.remove_attribute("number-rows-repeated")
+            base_row_range = base_row_range.add_next_sibling(row_range.dup)
+            base_row_range["table:number-rows-repeated"] = (rows_repeated - i)
+            cells = row_cells(base_row_range)
+            yield cells
+            @cells << cells
+            row_index += 1
+          end
+        end
       end
     end
 
@@ -50,6 +63,26 @@ module Rusk
       @cells.transpose.each do |columns|
         yield columns
       end
+    end
+
+    private
+    def row_cells(row_range)
+      row_cells = []
+      row_range.xpath(".//table:table-cell|.//table:covered-table-cell").each do |cell|
+        row_cells << Rusk::Cell.new(cell)
+
+        number_repeated = cell["table:number-columns-repeated"].to_i
+        if number_repeated > 1
+          cell.remove_attribute("number-columns-repeated")
+          base_cell = cell
+          (number_repeated - 1).times do
+            base_cell = base_cell.add_next_sibling(cell.dup)
+            row_cells << Rusk::Cell.new(base_cell)
+          end
+        end
+      end
+
+      row_cells
     end
 
   end
