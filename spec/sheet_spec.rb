2aea33eaa9788b5dc73f39ede92fb139fb7963b4
tomi <tomiacannondale@gmail.com>
2013-03-23 18:56:21 +0900

    Add test for sheet included number-rows-repeated

--- spec/sheet_spec.rb
+++ spec/sheet_spec.rb
@@ -3,60 +3,96 @@ require File.expand_path('spec_helper', File.dirname(__FILE__))
 
 describe Rusk::Sheet do
   before do
-    content = Nokogiri::XML(File.read("#{dir}/general_datas_content.xml"))
-    @sheet = Rusk::Sheet.new(content.xpath("//table:table")[0])
-    @cells = [
-      ["string", "mruby", ""],
-      ["date", Date.new(2012,04,29), ""],
-      ["time", "16:50", ""],
-      ["float", 7000.0, ""],
-      ["merged_cell", "", "after merged column"],
-      ["merged_row cell", "merged first row cell", ""],
-      ["", "merged second row cell", ""],
-      ["after merged row cell", "", ""],
-      ["", "", ""],
-      ["after blank row", "", ""],
-      ["", "after blank column", ""],
-      ["hide the cell next to", "hidden cell", ""],
-      ["percentage", 0.1, ""],
-      ["enter date of time", DateTime.new(2012,5,26,18,17), ""],
-      ["boolean", true, false],
-      ["currency", 10.0, ""]
-    ]
+    @content = Nokogiri::XML(File.read("#{dir}/general_datas_content.xml"))
+  end
+
+  shared_context "read Sheet1" do
+    before do
+      @sheet = Rusk::Sheet.new(@content.xpath("//table:table")[0])
+      @cells = [
+        ["string", "mruby", ""],
+        ["date", Date.new(2012,04,29), ""],
+        ["time", "16:50", ""],
+        ["float", 7000.0, ""],
+        ["merged_cell", "", "after merged column"],
+        ["merged_row cell", "merged first row cell", ""],
+        ["", "merged second row cell", ""],
+        ["after merged row cell", "", ""],
+        ["", "", ""],
+        ["after blank row", "", ""],
+        ["", "after blank column", ""],
+        ["hide the cell next to", "hidden cell", ""],
+        ["percentage", 0.1, ""],
+        ["enter date of time", DateTime.new(2012,5,26,18,17), ""],
+        ["boolean", true, false],
+        ["currency", 10.0, ""]
+      ]
+    end
+  end
+
+  shared_context "read rows-repeated" do
+    before do
+      @sheet = Rusk::Sheet.new(@content.xpath("//table:table")[1])
+      @cells = [
+        ["title1", "title2"],
+        ["", ""],
+        ["", ""],
+        ["", ""],
+        ["", ""],
+        ["", ""],
+        ["", ""],
+        ["", ""],
+        ["", ""],
+        ["", ""],
+        ["", ""],
+        ["", ""]
+      ]
+    end
   end
 
   describe "#name" do
+    include_context "read Sheet1"
     it { @sheet.name.should eq "Sheet1" }
   end
 
   describe "#[]" do
-    context "with exist cell index" do
-      it { @sheet[2, 1].should be_kind_of Rusk::Cell }
-      it { @sheet[0, 0].value.should eq "string" }
-      it { @sheet[0, 1].value.should eq "mruby" }
-      it { @sheet[1, 1].value.should eq Date.new(2012,4,29) }
-      it { @sheet[4, 2].value.should eq 'after merged column' }
-      it { @sheet[5, 1].value.should eq 'merged first row cell' }
-      it { @sheet[6, 1].value.should eq 'merged second row cell' }
-      it { @sheet[8, 0].value.should eq "" }
-      it { @sheet[8, 1].value.should eq "" }
-      it { @sheet[11, 0].value.should eq 'hide the cell next to' }
-      it { @sheet[11, 1].value.should eq 'hidden cell' }
+    context "read Sheet1(merged, columns-repeated, various data type)" do
+      include_context "read Sheet1"
+      context "with exist cell index" do
+        it { @sheet[2, 1].should be_kind_of Rusk::Cell }
+        it { @sheet[0, 0].value.should eq "string" }
+        it { @sheet[0, 1].value.should eq "mruby" }
+        it { @sheet[1, 1].value.should eq Date.new(2012,4,29) }
+        it { @sheet[4, 2].value.should eq 'after merged column' }
+        it { @sheet[5, 1].value.should eq 'merged first row cell' }
+        it { @sheet[6, 1].value.should eq 'merged second row cell' }
+        it { @sheet[8, 0].value.should eq "" }
+        it { @sheet[8, 1].value.should eq "" }
+        it { @sheet[11, 0].value.should eq 'hide the cell next to' }
+        it { @sheet[11, 1].value.should eq 'hidden cell' }
+      end
+
+      context "with not exist cell index" do
+        context "when row is existing, column is not existing" do
+          it { @sheet[0, 1000].should be_nil }
+        end
+
+        context "when row and column is not existing" do
+          it { @sheet[1000, 10000].should be_nil }
+        end
+      end
     end
 
-    context "with not exist cell index" do
-      context "when row is existing, column is not existing" do
-        it { @sheet[0, 1000].should be_nil }
-      end
-
-      context "when row and column is not existing" do
-        it { @sheet[1000, 10000].should be_nil }
-      end
+    context "read rows-repeated" do
+      include_context "read rows-repeated"
+      it { @sheet.map(&:value).should have(24).items }
+      it { @sheet.map(&:value).should eq @cells.flatten }
     end
 
   end
 
   describe "#each" do
+    include_context "read Sheet1"
     it "access order by row and column" do
       cells = @cells.flatten
       index = 0
@@ -68,6 +104,7 @@ describe Rusk::Sheet do
   end
 
   describe "#each_row" do
+    include_context "read Sheet1"
     it "access order by first row" do
       index = 0
       @sheet.each_row do |rows|
@@ -78,6 +115,7 @@ describe Rusk::Sheet do
   end
 
   describe "#each_column" do
+    include_context "read Sheet1"
     it "access order by first column" do
       index = 0
       cells = @cells.transpose
@@ -109,6 +147,46 @@ describe Rusk::Sheet do
 
         it { @book[1].name.should eq "changed" }
       end
+    end
+
+    context "read rows-repeated" do
+      before do
+        Rusk::Book.open(@tmp_file) do |book|
+          sheet = book[1]
+          sheet[1, 1].value = "modified 1,1"
+          sheet[2, 1].value = "modified 2,1"
+          sheet[3, 0].value = "modified 3,0"
+          sheet[3, 1].value = "modified 3,1"
+          sheet[11, 1].value = "modified 11,1"
+          sheet[5, 1].value = "modified 5,1"
+          book.save
+        end
+        @sheet = Rusk::Book.open(@tmp_file)[1]
+      end
+
+      it { @sheet[1,0].value.should eq "" }
+      it { @sheet[1,1].value.should eq "modified 1,1" }
+      it { @sheet[2,0].value.should eq "" }
+      it { @sheet[2,1].value.should eq "modified 2,1" }
+      it { @sheet[3,0].value.should eq "modified 3,0" }
+      it { @sheet[3,1].value.should eq "modified 3,1" }
+      it { @sheet[4,0].value.should eq "" }
+      it { @sheet[4,1].value.should eq "" }
+      it { @sheet[5,0].value.should eq "" }
+      it { @sheet[5,1].value.should eq "modified 5,1" }
+      it { @sheet[6,0].value.should eq "" }
+      it { @sheet[6,1].value.should eq "" }
+      it { @sheet[7,0].value.should eq "" }
+      it { @sheet[7,1].value.should eq "" }
+      it { @sheet[8,0].value.should eq "" }
+      it { @sheet[8,1].value.should eq "" }
+      it { @sheet[9,0].value.should eq "" }
+      it { @sheet[9,1].value.should eq "" }
+      it { @sheet[10,0].value.should eq "" }
+      it { @sheet[10,1].value.should eq "" }
+      it { @sheet[11,0].value.should eq "" }
+      it { @sheet[11,1].value.should eq "modified 11,1" }
+
     end
   end
 
